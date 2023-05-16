feafbd9e66329dea0234138ec529eae2178f8cc3
Sara Pandolfi <sara.pandolfi5@studio.unibo.it>
2023-05-16 16:46:46 +0100

    update parameter dictionaries and image path names

--- parameters.py
+++ parameters.py
@@ -1,29 +1,16 @@
 img_path = 'images'
-IMG_SIZE = 250
+test_img_path = 'test_images'
+IMG_SIZE = 300
 BATCH = 8
 EPOCHS = 5
 classes = ['BLA', 'EBO', 'MMZ', 'NGS']
 num_classes = 4
 
-augmentation_params = {
-    'rotation_range': 0,
-    'width_shift_range': 0.,
-    'height_shift_range': 0.,
-    'shear_range': 0.,
-    'zoom_range': 0.,
-    'horizontal_flip': False,
-    'vertical_flip': False,
-    'fill_mode': 'nearest',
-    'cval': 0.,
-    'validation_split': 0.10
-}
-
 train_params = {
-    'target_size': (IMG_SIZE, IMG_SIZE),
-    'color_mode': 'rgb',
-    'class_mode': 'categorical',
-    'batch_size': BATCH,
-    'shuffle': True,
-    'seed': 42
+'label_mode': 'categorical',
+'color_mode' : 'rgb',
+'batch_size' : BATCH,
+'image_size' : (IMG_SIZE, IMG_SIZE),
+'seed' : 42    
 }
 
