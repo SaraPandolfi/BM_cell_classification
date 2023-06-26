2d9532d0a65e968937693028398a49a47e4bd3fd
Sara Pandolfi <sara.pandolfi5@studio.unibo.it>
2023-06-26 11:08:22 +0200

    move files into new code folder

---
+++ classificationmodel/evaluation.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sklearn.metrics import classification_report
+from parameters import classes
+from classificationmodel.dataset import test_set
+from classificationmodel.model import load_model
+
+
+def evaluate_model(model, test_dataset, classes):
+    """
+    Evaluates the model on the test set, gets the predictions, 
+    print the classification report and returns metrics.
+
+    Parameters:
+        model (keras.Model): Trained model.
+        test_dataset (tf.data.Dataset): Test dataset.
+        classes (list): List of class names.
+
+    Returns:
+        Tuple containing the loss and accuracy.
+    """    
+    loss, accuracy = model.evaluate(test_dataset)
+
+    #Iterate over the test dataset to store true and predicted labels to get the classification report
+    y_true = []
+    y_pred = []
+    for x, y in test_dataset:
+        y_true.extend(np.argmax(y.numpy(), axis=1))
+        y_pred_probs = model.predict(x)
+        y_pred.extend(np.argmax(y_pred_probs, axis=1))
+    
+    print(classification_report(y_true, y_pred, target_names=classes))
+
+    return loss, accuracy
+
+if __name__ == '__main__':
+    model = load_model('model.pkl')
+
+    test_loss, test_accuracy = evaluate_model(model, test_set, classes)
+    print(f'Test Loss: {test_loss}')
+    print(f'Test Accuracy: {test_accuracy}')
+
