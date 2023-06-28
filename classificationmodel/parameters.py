aee04f42d0dd74abeaa844fd126cbe411b31faf2
Sara Pandolfi <sara.pandolfi5@studio.unibo.it>
2023-06-28 18:19:56 +0200

    added documentation to parameters

--- classificationmodel/parameters.py
+++ classificationmodel/parameters.py
@@ -1,3 +1,18 @@
+"""
+Module for storing parameters used in the modules.
+
+Parameters:
+    img_path (str): Path to the folder containing the training images.
+    test_img_path (str): Path to the folder containing the test images.
+    IMG_SIZE (int): Size of the images (width and height).
+    BATCH (int): Batch size for training.
+    EPOCHS (int): Number of training epochs.
+    classes (list): List of class labels.
+    num_classes (int): Number of classes.
+    train_params (dict): Dictionary of parameters for training the model.
+
+"""
+
 img_path = 'images'
 test_img_path = 'test_images'
 IMG_SIZE = 300
